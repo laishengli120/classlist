f7ac942d154f3d400b09d44920816664b0f606ad
laisheng <laisheng120@gmail.com>
2023-06-02 17:56:53 +0800

    Ready to deploy

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ from sqlalchemy.orm import validates
 from datetime import date
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
-# app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
-# app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
+# app.config['SECRET_KEY'] = 'dev'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config
 db = SQLAlchemy(app)
